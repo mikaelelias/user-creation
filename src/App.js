fe62394dd2bd867f95fdace5794ce57c236f1700
Mikael Elias <129218135+mikaelelias@users.noreply.github.com>
2024-02-17 22:05:22 -0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './style.js';
+import { 
+  Container,
+  Image,
+  ContainerItens,
+  H1,
+  InputLabel,
+  Input,
+  Button
+   } from './style.js';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+
+      <Image />
+      <ContainerItens>
+        <H1>Oi</H1>
+
+        <InputLabel>Nome</InputLabel>
+        <input placeholder="Nome"></input>
+
+        <InputLabel>Idade</InputLabel>
+        <input placeholder="Idade"></input>
+        
+        <button>Cadastrar</button>
+      </ContainerItens>
+
+    </Container>
   );
 }
 
